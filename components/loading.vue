0053557f4696c4a0f9ebf19c8a0b4f62db7caceb
deductive-test <118945657+deductive-test@users.noreply.github.com>
2022-11-24 13:46:00 +0900

    Add files via upload

---
+++ components/loading.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-overlay :value="overlayCopy">
+    <v-progress-circular
+      indeterminate
+      :size="size"
+      @click="overlayCopy = !overlayCopy"
+    />
+  </v-overlay>
+</template>
+
+<script>
+module.exports = {
+  name: 'Loading',
+  props: {
+    size: {
+      type: Number,
+      default: 200
+    },
+    overlay: {
+      type: Boolean,
+      default: false
+    }
+  },
+  data() {
+    return {
+      overlayCopy:false,
+    }
+  },
+  watch: {
+    'overlay'(to, from) {
+      this.overlayCopy = to
+    },
+  },
+  mounted() {
+    this.overlayCopy = this.overlay
+  },
+}
+</script>
+
+<style>
+</style>
